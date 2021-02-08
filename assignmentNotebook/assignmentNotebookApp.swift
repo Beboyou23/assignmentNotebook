fff23ba1ada056ed8ca9e356f73b75a5de59c1c1
Jordan Rarrat <Jrarrat@bsd220.org>
2021-02-08 11:07:07 -0600

    Initial Commit

---
+++ assignmentNotebook/assignmentNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentNotebookApp.swift
+//  assignmentNotebook
+//
+//  Created by Student on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct assignmentNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
