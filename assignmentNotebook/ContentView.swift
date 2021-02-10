0401e95558fff3208777ac57c7533446fd9f567f
Jordan Rarrat <Jrarrat@bsd220.org>
2021-02-09 22:34:33 -0600

    added navigation view, list and for each loop

--- assignmentNotebook/ContentView.swift
+++ assignmentNotebook/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var AssignmentItem = [""]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(AssignmentItem, id: \.self) { AssignmentItem in
+                    Text(AssignmentItem)
+                }
+                .onMove(perform: { indices, newOffset in
+                    AssignmentItem.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    AssignmentItem.remove(atOffsets: indexSet)
+                                })
+                
+            }
+            .navigationBarTitle("AssignmentItem")
+            .navigationBarItems(leading: EditButton())
+            
+        }
     }
 }
 
@@ -19,3 +35,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var  description = String()
+    var dueDate = Date()
+}
+
+
